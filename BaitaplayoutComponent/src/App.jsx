8da6c4fee906cb52179c93cbf9ddc338ef67c4e0
kangwu123 <kangwu123@users.noreply.github.com>
2025-10-26 20:24:33 +0700

    First Commit Include Stucture Project

---
+++ BaitaplayoutComponent/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import MainTemplate from './LayoutTemplate/MainTemplate'
+
+function App() {
+  return (
+    <div>
+       <MainTemplate />
+    </div>
+  )
+}
+
+export default App
---
+++ BaitaplayoutComponent/src/LayoutTemplate/MainTemplate.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MainTemplate = () => {
+  return (
+    <div>MainTemplate</div>
+  )
+}
+
+export default MainTemplate
